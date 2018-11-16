8427ea08d25c246930f1e461415b949b0022a207
trainee1 <trainee1@LAPSHA00001.fdmgroup.local>
2018-11-16 11:54:28 +0800

    added one test case

--- src/test/java/com/fdmgroup/Test/LoginControllerTest.java
+++ src/test/java/com/fdmgroup/Test/LoginControllerTest.java
@@ -2,22 +2,40 @@ package com.fdmgroup.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import com.fdmgroup.DAO.UserDao;
 import com.fdmgroup.Entities.User;
 import com.fdmgroup.spring.web.controller.LoginController;
 
 import static org.mockito.Mockito.*;
 
+import javax.persistence.EntityManagerFactory;
+
 
 public class LoginControllerTest {
+	
+	@Mock
+	private UserDao uDao;
+	
+	@InjectMocks
+	private LoginController lc = new LoginController();
+	
+	@Before
+	public void startInjectingMocks() {
+		
+		MockitoAnnotations.initMocks(this);
+	}
 	
 	@Test
 	public void when_requrestForLogin_then_returnLoginJsp(){
 		
 		//arrange
-		LoginController lc = new LoginController();
 		Model mockModel = mock(Model.class);
 		
 		//act
@@ -27,4 +45,25 @@ public class LoginControllerTest {
 		assertEquals("login", nextPage);
 		verify(mockModel).addAttribute("blank_login_user", new User());
 	}
+	
+	@Test
+	public void when_enterIncorrectUsername_then_returnLoginJspAndShowInvalidUsername() {
+		
+		//arrange
+		Model mockModel = mock(Model.class);
+		User mockUser = mock(User.class);
+		when(mockUser.getUsername()).thenReturn("none_existing_username");
+		when(uDao.get("none_existing_username")).thenReturn(null);
+		
+		//act
+		String nextPage = lc.loginUser(mockUser, mockModel);
+		
+		//
+		verify(uDao).get("none_existing_username");
+		verify(mockModel).addAttribute("blank_login_user", mockUser);
+		verify(mockModel).addAttribute("fail_msg", "invalid username");
+		assertEquals(nextPage, "login");
+	}
+	
+	
 }
